d9fc1da29272209a22601bd1b21bc9e5a1a172d3
alisonsavi64 <alisonsavi64@gmail.com>
2022-11-27 19:03:21 -0300

    arquivos atualizados

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
-const authController = require('./controllers/authController');
-const projectController = require('./controllers/projectController');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/auth', authController);
-app.use('/projects', projectController);
+require('./controllers/index')(app);
 
 app.listen(port, () => {console.log(`Server is listening on port ${port}`)});
