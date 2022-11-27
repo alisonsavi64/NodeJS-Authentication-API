a4c49a51ac5cb14b7c6a2fc6652c10d345434645
alisonsavi64 <alisonsavi64@gmail.com>
2022-11-27 16:59:14 -0300

    Banco de dados começado

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+require('dotenv').config()
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
-const userLogin = require('./routes/userLogin')
+const auth = require('./controllers/authController');
 
-app.use(express.static(__dirname + '/public'));
-app.set("view engine", "ejs");
-app.use('/login', userLogin);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/auth', auth);
 
 app.listen(port, () => {console.log(`Server is listening on port ${port}`)})
