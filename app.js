d0fc7a0203a39e5e76bf30067725cbedcdb39a50
alisonsavi64 <alisonsavi64@gmail.com>
2022-11-27 18:14:08 -0300

    autenticação quase completa

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
-const auth = require('./controllers/authController');
+const authController = require('./controllers/authController');
+const projectController = require('./controllers/projectController');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/auth', auth);
+app.use('/auth', authController);
+app.use('/projects', projectController);
 
-app.listen(port, () => {console.log(`Server is listening on port ${port}`)})
+app.listen(port, () => {console.log(`Server is listening on port ${port}`)});
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,22 @@
 const express = require('express');
 
-const User = require('../models/User')
+const User = require('../models/User');
+
+const bcrypt = require('bcrypt');
+
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth.json')
 
 const router = express.Router();
+
+function generateToken(params = {}){
+	return jwt.sign({ params }, authConfig.secret, {
+
+		expiresIn: 86400,
+
+	});
+}
 
 router.post('/register', async(req, res) => {
 
@@ -20,11 +34,43 @@ router.post('/register', async(req, res) => {
 
 		user.password = undefined;
 
-		return res.send({user});
+		return res.send({
+			user,
+			token: generateToken({id: user.id})
+
+		});
 	}catch (err) {
 		return res.status(400).send({error: 'Failed'})
 	}
 
 });
 
-module.exports = router
+router.post('/authenticate', async(req, res) => {
+
+	const { email, password } = req.body;
+
+	const user = await User.findOne({email}).select('+password');
+
+	if (!user){
+
+		return res.status(400).send({error: 'User not found'});
+
+	}
+
+	if (!await bcrypt.compare(password, user.password)){
+
+		return res.status(400).send({error: 'Invalid password'});
+
+	}
+
+	user.password = undefined;
+
+	
+
+	res.send({
+		user, 
+		token: generateToken({id: user.id})
+	});
+})
+
+module.exports = router;
